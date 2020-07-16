5e57b5be9f7f08c8b201a4a5b692d946c986c8d6
Abdul Basit <abdul.basit@veripark.com.pk>
2020-07-17 02:13:26 +0500

    Add Initail Screen

--- FlickerSearch/FlickerSearch/AppStartup/AppDelegate.swift
+++ FlickerSearch/FlickerSearch/AppStartup/AppDelegate.swift
@@ -13,16 +13,14 @@ import CoreData
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
+    let initialViewController = UITableViewController()
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         window = UIWindow(frame: UIScreen.main.bounds)
-        let initialViewController = UIViewController()
         window?.rootViewController = initialViewController
         window?.makeKeyAndVisible()
         return true
     }
-
 
     // MARK: - Core Data stack
 
